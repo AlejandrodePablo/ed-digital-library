637c0d822237e945ec4ed5d9d7c7068bfdcb975c
AlejandrodePablo <115726867+AlejandrodePablo@users.noreply.github.com>
2024-05-18 13:24:28 +0200

    test: Add unit tests for multiple functionalities (#30)
    - Add unit tests for NewUserUseCase, UpdateUserUseCase, DeleteUserUseCase and ListUserUseCase.
    - Verify the functionality of adding, updating, deleting and listing users in the system.
    - Include test cases for scenarios like missing, invalid, and data limit cases.
    - Ensure adequate test coverage for core functionalities of the system.
    
    #29

--- src/test/java/com/iesam/digitallibrary/features/user/domain/DeleteUserUseCaseTest.java
+++ src/test/java/com/iesam/digitallibrary/features/user/domain/DeleteUserUseCaseTest.java
@@ -2,20 +2,30 @@ package com.iesam.digitallibrary.features.user.domain;
 
 import com.iesam.digitallibrary.features.user.data.StubUserDataRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+@ExtendWith(MockitoExtension.class)
 class DeleteUserUseCaseTest {
 
-    private DeleteUserUseCase deleteUserUseCase;
-    private StubUserDataRepository stubUserDataRepository;
+    @Mock
+    UserRepository userRepository;
+
+    DeleteUserUseCase deleteUserUseCase;
+    StubUserDataRepository stubUserDataRepository;
 
     @BeforeEach
     void setUp() {
         stubUserDataRepository = new StubUserDataRepository();
         deleteUserUseCase = new DeleteUserUseCase(stubUserDataRepository);
+        deleteUserUseCase = new DeleteUserUseCase(userRepository);
     }
 
     @AfterEach
@@ -34,5 +44,43 @@ class DeleteUserUseCaseTest {
 
         // Then
         assertNull(stubUserDataRepository.getUser(userIdToDelete));
+    }
+
+    @Test
+    public void deleteAnUserThatExist() {
+        //Given
+        User userToDelete = new User("100", "98765432D", "Delete", "TestDelete", "td@example", "90", "987654321");
+        Mockito.doNothing().when(userRepository).deleteUser("100");
+
+        //When
+        deleteUserUseCase.execute("100");
+
+        //Then
+        Mockito.verify(userRepository).deleteUser(userToDelete.id);
+    }
+
+    @Test
+    public void whetherTheDeleteUserMethodIsBeingInvokedCorrectly() {
+        //Given
+        Mockito.doNothing().when(userRepository).deleteUser("100");
+
+        //When
+        deleteUserUseCase.execute("100");
+
+        //Then
+        Mockito.verify(userRepository, Mockito.times(1)).deleteUser("100");
+    }
+
+    @Test
+    public void ifIdIsNullThenItDoesNotTDelete() {
+        // Given
+        String userId = null;
+        //Could be replaced giving null at deleteUserUseCase.execute(null)
+
+        //When
+        deleteUserUseCase.execute(userId);
+
+        //
+        Mockito.verify(userRepository, Mockito.never()).deleteUser(Mockito.anyString());
     }
 }
